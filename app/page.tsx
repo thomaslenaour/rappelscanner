d0acc5706d89352ba95d81233050cd759302c7fb
Thomas Le Naour <lenaourthomas@gmail.com>
2023-12-28 22:14:26 +0100

    build: add turborepo

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import { ProductSection } from './components/product-section';
 
 export default function Home() {
   return (
-    <main className="max-w-2xl mx-auto my-16">
+    <main className="max-w-2xl mx-auto my-10 sm:my-16">
       <h1 className="font-bold text-3xl">
-        Scannez le code barre de votre produit, découvrez s&apos;il fait objet
-        d&apos;un rappel
+        Scannez le code barre de votre produit, découvrez s&apos;il fait
+        l&apos;objet d&apos;un rappel
       </h1>
       <p className="text-slate-600 mt-3">
         Chaque année, des centaines de produits font l&apos;objet de rappels à
