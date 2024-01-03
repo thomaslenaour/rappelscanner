77b61ee6caa9ebd1b55ed8744d7811256f537f13
Thomas Le Naour <lenaourthomas@gmail.com>
2024-01-03 15:39:53 +0100

    feat(background): change background color

---
+++ components/background.tsx
@@ -0,0 +1,7 @@
+export function Background() {
+  return (
+    <div className="fixed left-0 top-0 -z-10 h-full w-full">
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(34,197,94,0.13)_0,rgba(34,197,94,0)_50%,rgba(34,197,94,0)_100%)]"></div>
+    </div>
+  );
+}
