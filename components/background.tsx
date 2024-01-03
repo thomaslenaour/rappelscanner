dab78073e3778be95c40f58952629ab97dae7aa3
Thomas Le Naour <lenaourthomas@gmail.com>
2024-01-03 19:17:03 +0100

    feat(background): update background

--- components/background.tsx
+++ components/background.tsx
@@ -1,7 +1,9 @@
 export function Background() {
   return (
     <div className="fixed left-0 top-0 -z-10 h-full w-full">
-      <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(34,197,94,0.13)_0,rgba(34,197,94,0)_50%,rgba(34,197,94,0)_100%)]"></div>
+      <div className="relative h-full w-full bg-white">
+        <div className="absolute h-full w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]"></div>
+      </div>
     </div>
   );
 }
