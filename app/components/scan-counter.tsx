a0ae47f52457d50352e04562e2adbe3156bbe54c
Thomas Le Naour <lenaourthomas@gmail.com>
2024-01-05 15:20:48 +0100

    fix(counter): change html tag

--- app/components/scan-counter.tsx
+++ app/components/scan-counter.tsx
@@ -9,13 +9,13 @@ export async function ScanCounter({ className, ...props }: ScanCounterProps) {
 
   return (
     <div className={cn('', className)} {...props}>
-      <h2 className="font-bold text-2xl text-center">
+      <p className="font-bold text-2xl text-center">
         Déjà{' '}
         <span className="relative px-2 py-1 text-white before:bg-primary before:content-[''] before:absolute before:w-full before:h-full before:left-0 before:top-0 before:-z-10 before:-rotate-3">
           {scans || 0}
         </span>{' '}
         produits scannés
-      </h2>
+      </p>
     </div>
   );
 }
